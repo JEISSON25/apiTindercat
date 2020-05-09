9b5cd37dad188585f678bc5fd1f2578ef2ebe9e7
INGJEISSON <jibarguen@suyo.co>
2020-05-09 07:22:58 -0500

    Agregando interest model

--- routes/cats.js
+++ routes/cats.js
@@ -1,21 +1,22 @@
-var router = require('express').Router()
+var router = require("express").Router();
 const {
-  login, signup, catList,
-  liked, unliked, addInterest,
-  removeInterest, updatePreferences
-} = require('./../controllers/catsController')
+  login,
+  signup,
+  catList,
+  liked,
+  unliked,
+  addInterest,
+  removeInterest,
+  updatePreferences,
+} = require("./../controllers/catsController");
 
-router.get('/login', login)
-router.post('/signup', signup)
-router.get('/', catList) // devolver la lista de los gatos
-router.post('/liked', liked)
-router.post('/unliked', unliked)
-router.post('/interest', addInterest)
-router.delete('/interest', removeInterest)
-<<<<<<< HEAD
-router.put('preferences', updatePreferences)
-=======
-router.put('/preferences', updatePreferences)
->>>>>>> 2aaa5696a83cdac87952b8fdb6044f4510b1c0e0
+router.get("/login", login);
+router.post("/signup", signup);
+router.get("/", catList); // devolver la lista de los gatos
+router.post("/liked", liked);
+router.post("/unliked", unliked);
+router.post("/interest", addInterest);
+router.delete("/interest", removeInterest);
+router.put("/preferences", updatePreferences);
 
-module.exports = router
+module.exports = router;
--- services/cats/signup.js
+++ services/cats/signup.js
@@ -1,19 +1,12 @@
-const CatModel = require('../../models/cat')
-<<<<<<< HEAD
-const signup = async (catData) => {
-  // TODO
- const response = await CatModel(catData).save()
-  return { status: 1, response}
-=======
+const CatModel = require("../../models/cat");
 
 const signup = async (catData) => {
   try {
-    const response = await CatModel(catData).save()
-    return { status: 1, response }
+    const response = await CatModel(catData).save();
+    return { status: 1, response };
   } catch (err) {
-    return { status: 2, msg: 'Error saving document' }
+    return { status: 2, msg: "Error saving document" };
   }
->>>>>>> 2aaa5696a83cdac87952b8fdb6044f4510b1c0e0
-}
+};
 
-module.exports = signup
+module.exports = signup;
